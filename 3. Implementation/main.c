245ac79f51d3534fc4c2398962d59bc5a391b792
saikusal <66658250+saikusal@users.noreply.github.com>
2021-07-06 23:53:32 +0530

    Add files via upload

---
+++ 3. Implementation/main.c
@@ -0,0 +1,40 @@
+#include "calculator.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * @brief prototype functions
+ * 
+ * @param a  operand 1
+ * @param b  operand 2
+ * @return int  returns the output value
+ */
+int Addition(int a ,int b);
+
+
+int Subtraction(int a ,int b);
+
+
+int Multiplication(int a ,int b);
+
+
+int Division(int a ,int b);
+
+
+int Modulus(int a ,int b);
+
+
+int Power(int a ,int b);
+
+
+int Factorial(int a);
+
+int Square(int a);
+/*int main()
+{
+    printf(".............................\n");
+    printf("\t\t\t\t\t\tWELCOME\n");
+    printf("\t\t\t\t\t\t\t\t\t\t.....................\n");
+    return 0;
+}
+*/
